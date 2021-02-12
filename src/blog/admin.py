1cfdeb0f7ca6478a6cd55428fbdccbcb66e511be
Neata Dragos <dragos.neatax@gmail.com>
2021-02-12 10:01:17 +0200

    added comment model & admin view

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from . import models
 
+
+@admin.register(models.Post)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'slug', 'author')
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
 
-admin.site.register(models.Post, AuthorAdmin)
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'publish','status')
+    list_filter = ('status', 'publish')
+    search_field = ('name', 'email', 'content')
+
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'status', 'slug', 'author')
+
+# admin.site.register(models.Post, AuthorAdmin)
