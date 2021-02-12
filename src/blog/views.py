08770ca7674d9f9886854b909728d9b0ba288836
Neata Dragos <dragos.neatax@gmail.com>
2021-02-12 11:36:00 +0200

    CatListView added to url

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import Post
 from .forms import NewCommentForm
+from django.views.generic import ListView
 
 def home(request):
 
@@ -34,3 +35,15 @@ def post_single(request, post):
             'comment_form':comment_form
         },
     )
+
+
+class CatListView(ListView):
+    template_name = 'category.html'
+    context_object_name = 'catlist'
+
+    def get_queryset(self):
+        content = {
+            'cat' :self.kwargs['category'],
+            'posts': Post.objects.filter(category__name=self.kwargs['category']).filter(status='published')
+        }
+        return content
