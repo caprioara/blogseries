8cfaa9389d24734fe457891c4c39ebc1f0d5911c
Neata Dragos <dragos.neatax@gmail.com>
2021-02-15 21:40:43 +0200

    activation works

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
                                                 authentication_form=UserLoginForm), name='login'),
     path('profile/', views.profile, name='profile'),
     path('register/', views.accounts_register, name='register'),
-    # path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate')
+    path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
 ] 
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -43,3 +43,18 @@ def accounts_register(request):
         registerForm = RegistrationForm()
     return render(request, 'registration/register.html', {'form': registerForm})
     
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+        
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('login')
+    else:
+        return render(request, 'registration/activation_invalid.html')
