131d6b03380d1b41eceb6fe1dd3c8fdc6c822ade
Neata Dragos <dragos.neatax@gmail.com>
2021-02-16 17:58:36 +0200

    update bio + image

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import RegistrationForm, UserEditForm
+from .forms import RegistrationForm, UserEditForm, UserProfileForm
 from django.http import HttpResponse
+from .models import Profile
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -27,11 +28,17 @@ def delete_user(request):
 def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
-        if user_form.is_valid():
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
-    else: # we shod the form 
+            profile_form.save()
+
+    else: # we show the form 
         user_form = UserEditForm(instance=request.user)
-    return render(request, 'accounts/update.html', {'user_form': user_form})
+        profile_form = UserProfileForm(instance=request.user.profile)
+
+    return render(request, 'accounts/update.html', {'user_form': user_form, 'profile_form': profile_form})
 
 @login_required
 def profile(request):
